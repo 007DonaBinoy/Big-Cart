f2ace93892dfdfd571033644a034b4e537fb8a98
Dona Binoy <dona.binoy@trenser.com>
2024-11-10 11:13:01 +0530

    Initial Commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by User on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
